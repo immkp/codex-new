4d8bd51d7eeb1ca371b3106cbf7c651ae085459f
Manas Kumar Panigrahi <82246019+immkp@users.noreply.github.com>
2022-12-29 22:02:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,10 +15,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-  res.status(200).send({
-    message: 'Hello from CodeX!',
-  })
+// app.get('/', async (req, res) => {
+//   res.status(200).send({
+//     message: 'Hello from CodeX!',
+//   })
+// })
+
+app.get('/', (req, res) => {
+  res.sendStatus(200)
 })
 
 app.post('/', async (req, res) => {
